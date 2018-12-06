c4c4565c6ed78d4fdf203922819b6d69788f187f
BenGotts <45672919+BenGotts@users.noreply.github.com>
2018-12-06 13:45:12 -0800

    Add files via upload

---
+++ scrambleGenerator.py
@@ -0,0 +1,33 @@
+import random
+
+moves = ["U", "D", "F", "B", "R", "L"]
+dir = ["", "'", "2"]
+slen = random.randint(25, 28)
+
+def scramble_gen():
+    scramble = [0] * slen
+    for x in range(len(scramble)):
+        scramble[x] = [0] * 2
+    return scramble
+
+def scramble_replace(ar):
+    for x in range(len(ar)):
+        ar[x][0] = random.choice(moves)
+        ar[x][1] = random.choice(dir)
+    return ar
+
+def valid(ar):
+    for x in range(1, len(ar)):
+        while ar[x][0] == ar[x-1][0]:
+            ar[x][0] = random.choice(moves)
+    for x in range(2, len(ar)):
+        while ar[x][0] == ar[x-2][0] or ar[x][0] == ar[x-1][0]:
+            ar[x][0] = random.choice(moves)
+    return ar
+
+def sprint(ar):
+    for x in range(len(ar)):
+        print(str(ar[x][0]) + str(ar[x][1]), end = " ")
+
+s = scramble_replace(scramble_gen())
+sprint(valid(s))
