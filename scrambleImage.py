cf192f61639a1ee585f27982d88e025bb2daaa33
BenGotts <837gb1@gmail.com>
2020-05-14 20:23:33 -0700

    Test

--- scrambleImage.py
+++ scrambleImage.py
@@ -343,7 +343,9 @@ def scramble(scr, len):
     #                                             "    " + cube[5][6] + cube[5][5] + cube[5][4] + "\n"
     # )
     #
-    return
+    return cube
+
+
 
 import cv2 as cv
 import numpy as np
