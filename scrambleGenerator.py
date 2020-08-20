5e638fc0f6a38dc35fefde39291a7c2837676940
BenGotts <837gb1@gmail.com>
2020-08-19 17:14:16 -0700

    Updates

--- scrambleGenerator.py
+++ scrambleGenerator.py
@@ -1,24 +1,18 @@
 import random
 import sys
 from scrambleImage import scramble
-from scrambleImage import image
 
 moves = ["U", "D", "F", "B", "R", "L"]
 dir = ["", "'", "2"]
 slen = random.randint(25, 28)
 
 def gen_scramble():
-    # Make array of arrays that represent moves (U' = ['U', "'"])
+    # Make array of arrays that represent moves ex. U' = ['U', "'"]
     s = valid([[random.choice(moves), random.choice(dir)] for x in range(slen)])
     cube = scramble(s, slen)
 
     # Format scramble to a string with movecount
-    scr = ""
-    for x in range(len(s)):
-        scr += str(s[x][0]) + str(s[x][1]) + " "
-    scr += "[" + str(slen) + "]"
-
-    return scr
+    return ''.join(str(s[x][0]) + str(s[x][1]) + ' ' for x in range(len(s))) + "[" + str(slen) + "]"
 
 def valid(ar):
     # Check if Move behind or 2 behind is the same as the random move
@@ -31,11 +25,5 @@ def valid(ar):
             ar[x][0] = random.choice(moves)
     return ar
 
-def sprint(ar):
-    for x in range(len(ar)):
-        print(str(ar[x][0]) + str(ar[x][1]), end = " ")
-
 s = gen_scramble()
 print(s)
-if('-cv' in sys.argv):
-    image(s)
